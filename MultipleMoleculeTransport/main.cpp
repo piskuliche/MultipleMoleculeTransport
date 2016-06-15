97df1893a18647f241982a6126e9d38f3c2f84b0
piskuliche <piskuliche@ku.edu>
2016-06-15 14:57:06 -0500

    Updated Documentation
    Added some documentation

--- MultipleMoleculeTransport/main.cpp
+++ MultipleMoleculeTransport/main.cpp
@@ -11,12 +11,7 @@ int main()
 	/*---------------*/
 	//ints
 	int n_corr = 0;
-	vector<int> number_of_atoms;
-	vector<int> atoms_per_molecule;
-	vector<int> number_of_molecules;
-	vector<int> type;
 	int box_side_length = 0;
-	
 	int start_time = 0;
 	int end_time = 0;
 	int time_dump_freq = 0;
@@ -26,18 +21,18 @@ int main()
 	int lines = 0;
 	int number_of_species = 0;
 	int total_atoms = 0;
-
-	//takes the form of a vector of size # species, holding starting index for each species
-	//i.e. s1 always starts with 0, s2 starts with nbr_atoms[s1], etc.
-	vector<int> s_loc;
-
-	//doubles
-
 	//vectors
-	vector<atom> molecule;
-	vector<diffusion> diff;
-	vector<reorientation> reor;
-	vector<double> mass;
+	vector<atom> molecule; //Vector of class atom: Holds all atoms of all species (s1, s2..)
+	vector<diffusion> diff; // holds diffusion info
+	vector<reorientation> reor; //holds reorientation info
+	vector<double> mass; //temporary vector to hold mass. Unused past read_parameter().
+	vector<int> number_of_atoms; //Holds the number of atoms of each species (i.e. Nbr Molecs * atoms per molec)
+	vector<int> atoms_per_molecule; //Holds the atoms per molecule of each species (i.e. MeOH has "3")
+	vector<int> number_of_molecules; //Holds the number of molecules of each species
+	vector<int> type; //Holds the type of the molecule of each species
+	//currently 0 = EO, 1 = MeOH
+	vector<int> s_loc; //takes the form of a vector of size # species, holding starting index for each species i.e. s1 always starts with 0, s2 starts with nbr_atoms[s1], etc.
+
 	//strings
 	string filename = "default.dat";
 	string compound = "test";
